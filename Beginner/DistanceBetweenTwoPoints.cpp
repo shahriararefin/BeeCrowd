73a401832119ded2667cd5fac334c1d800916403
shahriararefin <shahriararefin7@gmail.com>
2024-05-31 18:53:34 +0600

    31/05/24

--- Beginner/DistanceBetweenTwoPoints.cpp
+++ Beginner/DistanceBetweenTwoPoints.cpp
@@ -4,15 +4,14 @@ using namespace std;
 
 int main(){
 
-    float x1, x2, y1, y2;
+    double x1, x2, y1, y2;
 
     cin >> x1 >> y1;
     cin >> x2 >> y2;
 
-    float x = x2-x1;
-    float y= y2 - y1;
+    
 
-    float Distance = 0.5*(pow(x,2)+ pow(y,2));
+    double Distance = sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
 
     cout << fixed << setprecision(4) << Distance << endl;
 
